b7294ecdfe7d571641c9434e37d71e085d53a5ce
Rachit Pareek <rachitpareek@berkeley.edu>
2020-11-25 00:53:07 -0800

    Created blog and business idea pages. Updated resume.

--- main.js
+++ main.js
@@ -1,24 +1,14 @@
 var words = ["Student.", "Developer.", "Entrepreneur."];
-var i = -1;
+var i = 0;
 
-function randomWord() {
-
+function wordCycler() {
     setInterval(function () {
-
-        i++
-
         var word = words[i];
-        console.log(words[i])
-
         $("#random-word").fadeOut('slow', function () {
             $(this).html(word).fadeIn("slow");
         });
-        
-        if (i === 2) {
-            i = -1;
-        }
-
+        i = (i+1)%3
     }, 3000);
 }
 
-randomWord();
+wordCycler();
